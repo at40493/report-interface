835965f29cddff2f700ddefe53c689940d2b2297
at40493 <at9247@gmail.com>
2018-11-21 16:47:24 +0800

    Move the paramter to the top and modify the name.

--- report_interface.sh
+++ report_interface.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+interface_name=$1
+interval_time=$2
+
 #Check the input format
-if [ -z "$1" -o -z "$2" ];
+if [ -z "${interface_name}" -o -z "${interval_time}" ];
 then
 	echo "Not enough argument."
 	echo "USAGE: ./report_interface.sh [interface name] [time(s)]"
@@ -9,7 +12,7 @@ then
 fi
 
 
-interface_name=$1
+
 # Check the interface name is exist.
 ifconfig ${interface_name} > /dev/null
 if [ $? -ne 0 ]; 
@@ -18,16 +21,16 @@ then
 	exit 0
 fi 
 
-delay_time=$2
+
 # Check the delay time is number.
-if [[ ! ${delay_time} =~ ^[0-9]+$ ]];
+if [[ ! ${interval_time} =~ ^[0-9]+$ ]];
 then
 	echo "The time is not a number."
 	exit 0;
 fi
 
 # Check the delay time is bigger than 0.
-if [ ${delay_time} -lt 0 ]; 
+if [ ${interval_time} -lt 0 ]; 
 then
 	echo "Time is negative, it must > 0."
 	exit 0;
@@ -37,5 +40,5 @@ fi
 while [ 1 ]; 
 do
 	grep ${interface_name} /proc/net/dev| awk '{printf("RX packets: %s TX packets: %s \r"),$3,$11}'
-	sleep ${delay_time}
+	sleep ${interval_time}
 done
